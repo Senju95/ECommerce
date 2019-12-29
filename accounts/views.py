e7f1842eaef5ce71abea4e34a3410e0972300620
Senju95 <josue.ad.95@gmail.com>
2019-12-28 21:50:18 -0600

    Custom User Model Completed

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login, get_user_model
+from django.views.generic import CreateView, FormView
 from django.utils.http import is_safe_url
 
 from .forms import LoginForm, RegisterForm, GuestForm
@@ -22,7 +23,39 @@ def guest_register_view(request):
         
     return redirect("/register/")
 
-def login_page(request):
+class LoginView(FormView):
+    form_class = LoginForm
+    success_url = '/'
+    template_name = 'accounts/login.html'
+
+    def form_valid(self, form):
+        request = self.request
+        next_ = request.GET.get('next')
+        next_post = request.POST.get('next')
+        redirect_path = next_ or next_post or None
+        user = authenticate(request, 
+                            username= form.cleaned_data.get("email"), 
+                            password= form.cleaned_data.get("password")
+                        )
+        if user is not None:
+            login(request, user)
+            try:
+                del request.session["guest_email_id"]
+            except:
+                pass
+            if is_safe_url(redirect_path, request.get_host()):
+                return redirect(redirect_path)
+
+            return redirect("/")
+        
+        return super(LoginView, self).form_invalid(form)
+
+class RegisterView(CreateView):
+    form_class = RegisterForm
+    template_name = 'accounts/register.html'
+    success_url = '/login/'
+
+"""def login_page(request):
     form = LoginForm(request.POST or None)
     context = {"form": form}
     next_ = request.GET.get('next')
@@ -46,17 +79,4 @@ def login_page(request):
         else:
             print("Error")
 
-    return render(request, "accounts/login.html", context)
-
-User = get_user_model()
-
-def register_page(request):
-    form = RegisterForm(request.POST or None)
-    context = {"form": form}
-    if form.is_valid():
-        username = form.cleaned_data.get("username")
-        email = form.cleaned_data.get("email") 
-        password = form.cleaned_data.get("password")
-        new_user = User.objects.create_user(username, email, password)
-        
-    return render(request, "accounts/register.html", context)
+    return render(request, "accounts/login.html", context)"""
